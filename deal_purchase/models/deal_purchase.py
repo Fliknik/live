5bc57157ed72ca26354c68b7d312b716ed97e125
Fliknik <86354682+Fliknik@users.noreply.github.com>
2022-05-27 11:19:13 +0500

    Add files via upload

--- deal_purchase/models/deal_purchase.py
+++ deal_purchase/models/deal_purchase.py
@@ -22,16 +22,29 @@ class DealPurchase(models.Model):
     def create(self, vals):
         vals['ref'] = self.env['ir.sequence'].next_by_code('deal.purchase') or _('New')  # get seq. like : 'SO111'
         reference = str(vals['ref']).replace('DE', '')
-        # your_new_so_name = 'DE' + '/' + str(date.today().strftime("%y/%m/%d")) + '/' + reference
+        if vals['date']:
+            print(type(vals['date']))
+            d1 = datetime.strptime(str(vals['date']), "%Y-%m-%d").date()
 
-        # my_string = '2019-10-31'
-        my_date = datetime.strptime(vals['date'], "%Y-%m-%d")
-        print(my_date.year)
-        print(my_date.month)
+            # my_date = datetime.strptime(vals['date'], "%Y-%m-%d")
+            # my_date = vals['date'].year
+            # print(type(vals['date']))
+            # print(my_date.month)
 
-        your_new_so_name = f'DE/{my_date.year}/{my_date.month}/{reference}'
-        vals.update({'ref': your_new_so_name})
+            your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference}'
+            vals.update({'ref': your_new_so_name})
         return super(DealPurchase, self).create(vals)
+
+    def write(self, vals):
+        vals['ref'] = self.env['ir.sequence'].next_by_code('deal.purchase') or _('New')  # get seq. like : 'SO111'
+        reference = str(vals['ref']).replace('DE', '')
+        if vals['date']:
+            print(type(vals['date']))
+
+            d1 = datetime.strptime(vals['date'], "%Y-%m-%d").date()
+            your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference}'
+            vals.update({'ref': your_new_so_name})
+        return super(DealPurchase, self).write(vals)
 
     def action_confirm(self):
         print("u click")
@@ -57,31 +70,40 @@ class DealLines(models.Model):
     product_id = fields.Many2one('product.template', string="Product")
     description = fields.Char('Description')
     qty = fields.Float('Quantity')
-    received_qty = fields.Float('Quantity Received', compute="_compute_received_quantity")
+    received_qty = fields.Float('Quantity Received' , compute = "_compute_received_quantity")
     unit_price = fields.Float('Unit Price')
     sub_total = fields.Float('SubTotal', readonly=True)
 
     deal_id = fields.Many2one('deal.purchase')
+
+
 
     @api.onchange('qty', 'unit_price', 'sub_total')
     def _function_subtotal(self):
         for rec in self:
             rec.sub_total = rec.qty * rec.unit_price
 
+    @api.depends('received_qty')
     def _compute_received_quantity(self):
         for rec in self:
-            record = self.env['stock.move'].search([('product_id.name', '=', rec.product_id.name)])
-            for r in record:
-                print(r.quantity_done)
-                rec.received_qty = r.quantity_done
-                break
-            # for r in record.move_ids_without_package:
-
-            # print(record)            # for r in record:
-            #     print(r)
+            result = self.env['deal.purchase'].browse(self.env.context.get('active_ids'))
+            print(result)
+            record = self.env['stock.picking'].search([('deal_id', '=', rec.deal_id.id)])
+            print(record)
+            r = 0
+            for l in record.move_ids_without_package:
+                if l.product_id.id == rec.product_id.id:
+                    r = r+l.quantity_done
+            rec.received_qty = r
 
 
 class SaleOrder(models.Model):
     _inherit = "purchase.order"
+
+    deal_id = fields.Many2one('deal.purchase', string='Deals')
+
+
+class StockPickingField(models.Model):
+    _inherit = "stock.picking"
 
     deal_id = fields.Many2one('deal.purchase', string='Deals')
