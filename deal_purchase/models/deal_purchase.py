666593c55e6cdbf59778fefc372826a426ca817a
Fliknik <86354682+Fliknik@users.noreply.github.com>
2022-05-27 15:11:11 +0500

    Add files via upload

--- deal_purchase/models/deal_purchase.py
+++ deal_purchase/models/deal_purchase.py
@@ -1,7 +1,7 @@
 from odoo import api, models, fields, _
-from datetime import date
 from odoo.exceptions import UserError, ValidationError
 from datetime import datetime
+
 
 class DealPurchase(models.Model):
     _name = 'deal.purchase'
@@ -11,7 +11,7 @@ class DealPurchase(models.Model):
     ref = fields.Char(string='Order Reference', required=True, copy=False, readonly=True, default=lambda self: _('New'),
                       tracking=True)
     vendor_id = fields.Many2one('res.partner', string="Vendor", tracking=True)
-    date = fields.Date(string='Date', tracking=True)
+    date = fields.Date(string='Date', tracking=True, required=True)
     vendor_reference = fields.Char(string='Vendor Reference', tracking=True)
     state = fields.Selection([('draft', 'Draft'), ('confirm', 'confirmed'), ('cancel', 'Cancelled')], default='draft',
                              string="status", tracking=True)
@@ -22,32 +22,22 @@ class DealPurchase(models.Model):
     def create(self, vals):
         vals['ref'] = self.env['ir.sequence'].next_by_code('deal.purchase') or _('New')  # get seq. like : 'SO111'
         reference = str(vals['ref']).replace('DE', '')
-        if vals['date']:
-            print(type(vals['date']))
-            d1 = datetime.strptime(str(vals['date']), "%Y-%m-%d").date()
-
-            # my_date = datetime.strptime(vals['date'], "%Y-%m-%d")
-            # my_date = vals['date'].year
-            # print(type(vals['date']))
-            # print(my_date.month)
-
-            your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference}'
-            vals.update({'ref': your_new_so_name})
+        d1 = datetime.strptime(str(vals['date']), "%Y-%m-%d").date()
+        your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference}'
+        vals.update({'ref': your_new_so_name})
         return super(DealPurchase, self).create(vals)
 
     def write(self, vals):
-        vals['ref'] = self.env['ir.sequence'].next_by_code('deal.purchase') or _('New')  # get seq. like : 'SO111'
-        reference = str(vals['ref']).replace('DE', '')
-        if vals['date']:
-            print(type(vals['date']))
-
-            d1 = datetime.strptime(vals['date'], "%Y-%m-%d").date()
-            your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference}'
+        if 'date' in vals:
+            print(self.ref)
+            reference = self.ref.split('/')
+            print(reference[3])
+            d1 = datetime.strptime(str(vals['date']), "%Y-%m-%d")
+            your_new_so_name = f'DE/{d1.year}/{d1.month}/{reference[3]}'
             vals.update({'ref': your_new_so_name})
         return super(DealPurchase, self).write(vals)
 
     def action_confirm(self):
-        print("u click")
         self.state = 'confirm'
 
     def action_reset_draft(self):
@@ -70,13 +60,11 @@ class DealLines(models.Model):
     product_id = fields.Many2one('product.template', string="Product")
     description = fields.Char('Description')
     qty = fields.Float('Quantity')
-    received_qty = fields.Float('Quantity Received' , compute = "_compute_received_quantity")
+    received_qty = fields.Float('Quantity Received', compute="_compute_received_quantity")
     unit_price = fields.Float('Unit Price')
     sub_total = fields.Float('SubTotal', readonly=True)
 
     deal_id = fields.Many2one('deal.purchase')
-
-
 
     @api.onchange('qty', 'unit_price', 'sub_total')
     def _function_subtotal(self):
@@ -93,7 +81,7 @@ class DealLines(models.Model):
             r = 0
             for l in record.move_ids_without_package:
                 if l.product_id.id == rec.product_id.id:
-                    r = r+l.quantity_done
+                    r = r + l.quantity_done
             rec.received_qty = r
 
 
