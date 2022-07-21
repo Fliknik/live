b3a42ef7a0739de956e2e4f7c702b9349eb23b12
Fliknik <86354682+Fliknik@users.noreply.github.com>
2022-07-21 14:56:01 +0500

    Add files via upload

--- picking_fields/models/models.py
+++ picking_fields/models/models.py
@@ -6,6 +6,6 @@ from odoo import models, fields, api
 class StockPickingInh(models.Model):
     _inherit = 'stock.picking'
 
-    grv_no = fields.Char('GRV #')
-    invoice_no = fields.Char('Invoice #')
-    delivery_note = fields.Char('Delivery Note #')
+    grv_no = fields.Char('GRV #', tracking=True)
+    invoice_no = fields.Char('Invoice #', tracking=True)
+    delivery_note = fields.Char('Delivery Note #', tracking=True)
